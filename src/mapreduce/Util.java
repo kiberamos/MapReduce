3dba2b44584923a75e116fbdab9574f87a1f1375
Kiberamos <kiberamos@hotmail.com>
2019-09-07 10:33:02 -0500

    Create Util.java

---
+++ src/mapreduce/Util.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mapreduce;
+
+/**
+ *
+ * @author wcade
+ */
+public class Util {
+    public static void print(String linea){
+        System.out.println(linea);
+    }
+    public static void println(){
+        Util.print(linea);
+    }
+}
