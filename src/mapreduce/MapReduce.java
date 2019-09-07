6a4775b9cab5b5aca02b37a1af49e7638d55d53b
Kiberamos <kiberamos@hotmail.com>
2019-09-07 11:41:46 -0500

    Cambio de utils

--- src/mapreduce/MapReduce.java
+++ src/mapreduce/MapReduce.java
@@ -5,7 +5,15 @@
  */
 package mapreduce;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import static java.lang.Thread.sleep;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 /**
  *
@@ -13,7 +21,16 @@ import static java.lang.Thread.sleep;
  */
 public class MapReduce {
 
-    private static String numeroArchivosGenerados;
+    /** util de lectura y escritudra de lista de archivos
+     * 
+     */
+    static List<String> archivosPath = new ArrayList<String>();
+        
+    /**
+     * contador
+     */
+    static int numeroArchivosGenerados = 0;
+    final static int NUMERO_PARRAFOS = 25;
 
     /**
      * @param args the command line arguments
@@ -21,10 +38,7 @@ public class MapReduce {
     
     public static void main(String[] args) throws InterruptedException {
 
-        
-        Util.println("Se han creado  Mappers");
-        crearHilosReducers();
-        Util.println("Se han creado  Reducers\n******************************");
+        Util.println("Se han creado  Reducers");
         crearHilosMap();
         
 
@@ -39,6 +53,59 @@ public class MapReduce {
 
         Map m = new Map();
 
+    }
+     private static void leer(String path) {
+
+        File archivo = new File(path);
+        Scanner s = null;
+
+        try {
+
+            s = new Scanner(archivo);
+            int contador = 0;
+            int auxLineas = 0;
+            String parrafos = "";
+
+            while (s.hasNextLine()) {
+                String linea = s.nextLine();
+                auxLineas++;
+                contador += linea.split("\n").length;
+                parrafos += linea;
+                if (contador == NUMERO_PARRAFOS) {
+                    escribir(parrafos, auxLineas);
+                    auxLineas = 0;
+                    parrafos = "";
+                    contador = 0;
+                }
+            }
+
+            if (contador < NUMERO_PARRAFOS) escribir(parrafos, auxLineas);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (s != null) s.close();
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
+    }
+     
+     private static void escribir(String texto, int numeroLineas) {
+
+        try {
+
+            File temp = File.createTempFile("temp" + numeroArchivosGenerados++ + "_" + numeroLineas + "_", ".txt", Paths.get("temp").toFile());
+
+            FileWriter archivo = new FileWriter(temp);
+            archivo.write(texto);
+            archivo.close();
+            archivosPath.add(temp.getPath());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     
 }
